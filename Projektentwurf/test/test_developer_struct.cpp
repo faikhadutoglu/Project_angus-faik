6af074cc1172cc693c4d5fcd9575e8acfd0bf1d2
faikhadutoglu <167749575+faikhadutoglu@users.noreply.github.com>
2024-06-07 19:28:30 +0200

    added a test for wonder women logo
    added one more test for wonder woman

--- Projektentwurf/test/test_developer_struct.cpp
+++ Projektentwurf/test/test_developer_struct.cpp
@@ -17,3 +17,7 @@ TEST(DeveloperTest, sdTestLoadLogo) {
     JuniorDeveloper jd("Peter Parker", "Spiderman");
     EXPECT_THROW(jd.load_logo_from_file("invalid_path"), std::runtime_error);
 }
+TEST(DeveloperTest, sdTestLoadLogoInvalidPath) {
+    SeniorDeveloper sd("Diana Prince", "Wonder Woman");
+    EXPECT_THROW(sd.load_logo_from_file("invalid_path"), std::runtime_error);
+}
